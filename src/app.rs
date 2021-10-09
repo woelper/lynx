7f4b9bb9dbdf146a979def7376d901ba0d8a6bef
Johann Woelper <woelper@gmail.com>
2021-10-10 00:01:16 +0200

    arc

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,11 @@
 use anyhow::{anyhow, Result};
-use std::{
-    fs::File,
-    io::BufReader,
-    path::{Path, PathBuf},
-    time::Duration,
-};
+use std::{fs::File, io::BufReader, path::{Path, PathBuf}, sync::Arc, time::Duration};
 
-use eframe::{
-    egui::{self},
-    epi,
-};
+use kira::{instance::{InstanceSettings, PauseInstanceSettings, ResumeInstanceSettings, handle::InstanceHandle}, manager::AudioManager, sound::{handle::SoundHandle, SoundSettings}};
+
+use eframe::{egui::{self, Label}, epi};
 use std::ffi::OsStr;
 use structopt::StructOpt;
-
-use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink, Source, source::Buffered};
 
 #[cfg(feature = "persistence")]
 use serde::{Deserialize, Serialize};
@@ -21,13 +13,15 @@ use serde::{Deserialize, Serialize};
 type SoundQueue = Vec<Sound>;
 
 #[cfg_attr(feature = "persistence", derive(Deserialize, Serialize))]
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Sound {
     pub path: PathBuf,
     pub sample_rate: u32,
     pub channels: u16,
     pub duration: Option<Duration>,
     pub looped: bool,
+    #[serde(skip)]
+    pub handle: Option<SoundHandle>,
 }
 
 impl Sound {
@@ -38,16 +32,10 @@ impl Sound {
         }
     }
 
-    pub fn generate_source(&mut self) -> Result<Buffered<Decoder<File>>> {
-        let open_file = File::open(&self.path)?;
-        // let r = BufReader::new(open_file);
-        // Decode that sound file into a source
-        let source = Decoder::new(open_file)?.buffered();
-        dbg!(source.current_frame_len());
-        self.sample_rate = source.sample_rate();
-        self.channels = source.channels();
-        self.duration = source.total_duration();
-        Ok(source)
+    pub fn generate_source(&mut self, manager: &mut AudioManager) -> Result<()> {
+        let sound_handle = manager.load_sound(&self.path, SoundSettings::default())?;
+        self.handle = Some(sound_handle);
+        Ok(())
     }
 }
 
@@ -61,11 +49,12 @@ struct Opt {
 
 #[cfg_attr(feature = "persistence", derive(Deserialize, Serialize))]
 pub struct ApplicationState {
-
     #[serde(skip)]
-    pub streams: Option<(OutputStream, OutputStreamHandle)>,
+    pub manager: Option<AudioManager>,
     #[serde(skip)]
-    pub sink: Option<Sink>,
+    active_sound: Option<SoundHandle>,
+    #[serde(skip)]
+    active_instance: Option<Arc<InstanceHandle>>,
     volume: f32,
     queue: SoundQueue,
     queue_index: usize,
@@ -74,8 +63,9 @@ pub struct ApplicationState {
 impl Default for ApplicationState {
     fn default() -> Self {
         Self {
-            streams: None,
-            sink: None,
+            manager: None,
+            active_sound: None,
+            active_instance: None,
             volume: 1.0,
             queue: vec![],
             queue_index: 0,
@@ -95,28 +85,36 @@ impl epi::App for ApplicationState {
         _frame: &mut epi::Frame<'_>,
         storage: Option<&dyn epi::Storage>,
     ) {
+        use kira::{instance::InstanceSettings, manager::AudioManagerSettings};
+
         if let Some(storage) = storage {
             let storage: ApplicationState =
                 epi::get_value(storage, epi::APP_KEY).unwrap_or_default();
             *self = storage;
-            self.streams = OutputStream::try_default().ok();
-            self.sink = Sink::try_new(&self.streams.as_ref().unwrap().1).ok();
         }
 
         // Parse arguments to auto-play sound
         let args = Opt::from_args();
+        
+        self.manager = AudioManager::new(AudioManagerSettings::default()).ok();
 
         if let Some(first_arg) = args.files.first() {
             let mut sound = Sound::default().with_path(first_arg);
-            if let Ok(source) = sound.generate_source() {
-                self.queue.push(sound);
-                if let Some(sink) = &self.sink {
-                    sink.append(source);
+            if let Some(manager) = &mut self.manager {
+                self.active_sound = manager.load_sound(first_arg, SoundSettings::default()).ok();
+                if let Some(active_sound) = &mut self.active_sound {
+
+                    let inst = active_sound.play(InstanceSettings::default()).ok();
+                    self.active_instance = Some(Arc::new(inst.unwrap()));
                 }
+                // let x  = sound_handle.play(InstanceSettings::default()).ok();
+
+                // if let Ok(_) = sound.generate_source(manager) {
+                //     self.active_sound =
+                //         sound.handle.unwrap().play(InstanceSettings::default()).ok();
+                //         dbg!(&self.active_sound);
+                // }
             }
-        }
-        if let Some(sink) = &self.sink {
-            sink.set_volume(self.volume);
         }
     }
 
@@ -125,10 +123,11 @@ impl epi::App for ApplicationState {
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
         let ApplicationState {
-            streams,
-            sink,
+            manager,
+            active_sound,
+            active_instance,
             volume,
             queue,
             queue_index,
@@ -159,55 +158,61 @@ impl epi::App for ApplicationState {
                 }
             }
 
-            if let Some(current_song) = queue.get(*queue_index) {
-                ui.label(format!("{} kHz, {} channels {:?}", current_song.sample_rate, current_song.channels, current_song.duration.unwrap_or_default()));
-            }
+            if let Some(manager) = manager {
+                if let Some(current_song) = queue.get(*queue_index) {
+                    ui.label(format!(
+                        "{} kHz, {} channels {:?}",
+                        current_song.sample_rate,
+                        current_song.channels,
+                        current_song.duration.unwrap_or_default()
+                    ));
 
-            if let Some(sink) = sink {
-                ui.horizontal(|ui| {
-                    if sink.is_paused() {
-                        if ui.button("Play").clicked() {
-                            sink.play();
-                        }
-                    } else {
-                        if ui.button("Pause").clicked() {
-                            sink.pause();
-                        }
-                    }
-                    if ui.button("Stop").clicked() {
-                        sink.stop();
-                    }
-                });
-
-                if ui
+                    if ui
                     .add(egui::Slider::new(volume, 0.0..=3.0).text("volume"))
                     .changed()
-                {
-                    sink.set_volume(*volume);
+                    {
+                        let _ = manager.main_track().set_volume(*volume as f64);
+                    }
                 }
 
+                if let Some(active_sound) = active_sound {
+                    // ui.label(format!("Pos {}", active_sound.));
 
+                    ui.horizontal(|ui| {
+                        if ui.button("Play").clicked() {
+                            let _ = active_sound.resume(ResumeInstanceSettings::new());
+                            // active_sound.play(InstanceSettings::new()).unwrap();
+                        }
+
+                        if ui.button("Pause").clicked() {
+                            let _ = active_sound.pause(PauseInstanceSettings::new());
+                        }
+
+                        if ui.button("Stop").clicked() {
+
+                        }
+                    });
+                    if let Some(active_instance) = active_instance {
+                        let pos = active_instance.position();
+                        
+                        ui.label(format!("pos {:.1} {:.1}% {:.1}", pos, pos/active_sound.duration(), active_sound.duration()));
+                    }
+                }
+
+        
 
                 ui.vertical_centered_justified(|ui| {
                     for (i, sound) in queue.iter_mut().enumerate() {
-
-                        if ui.selectable_label(*queue_index == i, nice_name(&sound.path)).double_clicked() {
+                        if ui
+                            .selectable_label(*queue_index == i, nice_name(&sound.path))
+                            .double_clicked()
+                        {
                             *queue_index = i;
-                            if let Ok(source) = sound.generate_source() {
+                            if let Ok(source) = sound.generate_source(manager) {
                                 dbg!("loaded", &sound);
                                 // due to rodio's design, the sink is lost after stopping
-                                *sink = Sink::try_new(&streams.as_ref().unwrap().1).unwrap();
-                                sink.set_volume(*volume);
-                                let len = source.size_hint().0;
-                                dbg!("hint", len);
-                                let s = source.periodic_access(Duration::from_secs(1), |x| {
-                                    // dbg!(x.);
-                                });
-                                sink.append(s);
-                                sink.play();
                             }
                         }
-
                     }
                 });
             }
@@ -215,14 +220,13 @@ impl epi::App for ApplicationState {
     }
 }
 
-fn nice_name(p: &Path) ->String {
+fn nice_name(p: &Path) -> String {
     format!(
         "{}",
-        p
-        .file_name()
-        .unwrap_or(OsStr::new("no path"))
-        .to_string_lossy()
-        .replace("_", " ")
-        .replace("-", " ")
+        p.file_name()
+            .unwrap_or(OsStr::new("no path"))
+            .to_string_lossy()
+            .replace("_", " ")
+            .replace("-", " ")
     )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod app;
+use app::Sound;
 use eframe::epi;
+use kira::{instance::InstanceSettings, manager::{AudioManager, AudioManagerSettings}, sound::SoundSettings};
 use std::path::PathBuf;
 use structopt::StructOpt;
-
-use rodio::{source::Source, Decoder, OutputStream};
-use std::fs::File;
-use std::io::BufReader;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
@@ -16,25 +14,18 @@ struct Opt {
 }
 
 fn main() {
-    // let args = Opt::from_args();
-    // let input_files = args.files;
-
-    // Get a output stream handle to the default physical sound device
-    //let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    // let file = BufReader::new(File::open(&input_files.first().unwrap()).unwrap());
-    // // Decode that sound file into a source
-    // let source = Decoder::new(file).unwrap();
-
-    // dbg!(&source.channels());
-
     let app = app::ApplicationState::default();
-    // app.active_sound = Some(source);
+    let opts = epi::NativeOptions::default();
 
-    // if app.active_sound.is_some() {
-    //     dbg!("some sound");
+
+    // let args = Opt::from_args();
+    // if let Some(first_arg) = args.files.first() {
+    //     let mut audio_manager = AudioManager::new(AudioManagerSettings::default()).unwrap();
+    //     let mut sound_handle = audio_manager.load_sound(first_arg, SoundSettings::default()).unwrap();
+    //         sound_handle.play(InstanceSettings::default()).unwrap();
+    //         std::thread::sleep(std::time::Duration::from_secs(10));
     // }
 
-    let opts = epi::NativeOptions::default();
+
     eframe::run_native(Box::new(app), opts);
 }
