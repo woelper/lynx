2b763fdb6b84268b10f71ce64e32fe5ae62b8e61
Johann Woelper <woelper@gmail.com>
2021-10-15 14:58:02 +0200

    change location

--- build_mac.sh
+++ build_mac.sh
@@ -1,4 +1,4 @@
 cargo bundle --release
 #cargo bundle
 #cp res/info.plist target/debug/bundle/osx/oculante.app/Contents/Info.plist
-cp res/info.plist target/release/bundle/osx/lynx.app/Contents/Info.plist
+cp src/info.plist target/release/bundle/osx/lynx.app/Contents/Info.plist
